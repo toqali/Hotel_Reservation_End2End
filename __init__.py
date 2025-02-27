6d46771d84e591e6af6accbb2eef7bd921289598
Toqa Ghozlan <156839271+toqali@users.noreply.github.com>
2025-02-27 23:02:27 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,8 @@
+import data
+from .evaluate_models import *
+from .models_architecture import *
+from .predict_models import *
+from .saved_model import *
+from .saved_preprocessings import *
+from .train_models import *
+
